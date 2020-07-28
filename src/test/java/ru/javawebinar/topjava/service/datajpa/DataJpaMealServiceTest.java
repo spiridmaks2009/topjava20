c1d00c31ba192383ce2ef0ad9cd9c27573e2d5ed
spiridmaks2009 <spiridmaks@gmail.com>
2020-07-28 16:05:58 +0300

    7_15 - 7_16 patches

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -14,6 +14,7 @@ import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
 @ActiveProfiles(DATAJPA)
 class DataJpaMealServiceTest extends AbstractMealServiceTest {
+
     @Test
     void getWithUser() throws Exception {
         Meal adminMeal = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
